c3a54e959c12f086623de83d4ecc0990e6a38882
Andey Amelchenia <pmad1986@gmail.com>
2021-05-21 17:14:40 +0300

    fix: add export db

--- db.js
+++ db.js
@@ -9,8 +9,9 @@ sequelize.authenticate().then(
     function success() {
         console.log("Connected to DB");
     },
-
     function fail(err) {
         console.log(`Error: ${err}`);
     }
 )
+
+module.exports = sequelize

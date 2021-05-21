65bef3bec8f77891da02c65add0bfbf314bc181b
Andey Amelchenia <pmad1986@gmail.com>
2021-05-21 17:16:10 +0300

    fix: it's all

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-var User = require('sequelize').import('../models/user');
+var User = require('../db').define('../models/user');
 
 module.exports = function (req, res, next) {
     if (req.method == 'OPTIONS') {
@@ -9,8 +9,10 @@ module.exports = function (req, res, next) {
         console.log(sessionToken);
         if (!sessionToken) return res.status(403).send({ auth: false, message: "No token provided." });
         else {
-            jwt.verify(sessionToken, 'lets_play_sum_games_man', (err, decoded) => {
+            jwt.verify(sessionToken, 'lets_play_sum_games_man',(err, decoded) => {
+                console.log(`decoded`, decoded, err);
                 if (decoded) {
+                    console.log(decoded);
                     User.findOne({ where: { id: decoded.id } }).then(user => {
                         req.user = user;
                         console.log(`user: ${user}`)
